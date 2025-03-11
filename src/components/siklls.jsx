694b9bd1830df87e2af25e292f6e0adc89ee70e6
SafarovS <sukhrob2024@gmail.com>
2025-03-11 09:56:33 +0500

    Ozgarishlar kritildi

---
+++ src/components/siklls.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect } from "react";
+import '../assets/css/main.css'
+
+function Siklls() {
+    useEffect(() => {
+        document.querySelectorAll(".progress-bar").forEach((bar) => {
+            bar.style.width = "0%"; // Dastlab width ni 0% qilamiz
+
+            setTimeout(() => {
+                const width = bar.getAttribute("aria-valuenow") + "%";
+                bar.style.width = width;
+            }, 300); // 300ms keyin animatsiyani boshlaymiz
+        });
+    }, []);
+    return (
+        <>
+            <section id="skills" className="skills section">
+                <div className="container section-title" data-aos="fade-up">
+                    <h2>Skills</h2>
+                    <div><span>My</span> <span className="description-title">Skills</span></div>
+                </div>
+                <div className="container" data-aos="fade-up" data-aos-delay="100">
+                    <div className="row skills-content skills-animation">
+                        <div className="col-lg-6">
+                            <div className="progress">
+                                <span className="skill"><span>HTML</span> <i className="val">100%</i></span>
+                                <div className="progress-bar-wrap">
+                                    <div className="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                            <div className="progress">
+                                <span className="skill"><span>CSS</span> <i className="val">90%</i></span>
+                                <div className="progress-bar-wrap">
+                                    <div className="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                            <div className="progress">
+                                <span className="skill"><span>JavaScript</span> <i className="val">75%</i></span>
+                                <div className="progress-bar-wrap">
+                                    <div className="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="progress">
+                                <span className="skill"><span>PHP</span> <i className="val">80%</i></span>
+                                <div className="progress-bar-wrap">
+                                    <div className="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                            <div className="progress">
+                                <span className="skill"><span>WordPress/CMS</span> <i className="val">90%</i></span>
+                                <div className="progress-bar-wrap">
+                                    <div className="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                            <div className="progress">
+                                <span className="skill"><span>React</span> <i className="val">55%</i></span>
+                                <div className="progress-bar-wrap">
+                                    <div className="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Siklls;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,70 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Stats from "../components/aboutstats.jsx";
+import Siklls from "../components/siklls.jsx";
 function About() {
   return (
     <>
-      <h1>About page</h1>
+      <div className="page-title" data-aos="fade">
+        <div className="heading">
+          <div className="container">
+            <div className="row d-flex justify-content-center text-center">
+              <div className="col-lg-8">
+                <h1>About</h1>
+                <p className="mb-0">Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo odio sint voluptas consequatur ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione sint. Sit quaerat ipsum dolorem.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <nav className="breadcrumbs">
+          <div className="container">
+            <ol>
+              <li><Link to="/">Home</Link></li>
+              <li className="current">About</li>
+            </ol>
+          </div>
+        </nav>
+      </div>
+      <section id="about" className="about section">
+        <div className="container" data-aos="fade-up" data-aos-delay="100">
+          <div className="row gy-4 justify-content-center">
+            <div className="col-lg-4">
+              <img src="/img/profile-img.jpg" className="img-fluid" alt="" />
+            </div>
+            <div className="col-lg-8 content">
+              <h2>UI/UX Designer &amp; Web Developer.</h2>
+              <p className="fst-italic py-3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua.
+              </p>
+              <div className="row">
+                <div className="col-lg-6">
+                  <ul>
+                    <li><i className="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
+                    <li><i className="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
+                    <li><i className="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
+                    <li><i className="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
+                  </ul>
+                </div>
+                <div className="col-lg-6">
+                  <ul>
+                    <li><i className="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
+                    <li><i className="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
+                    <li><i className="bi bi-chevron-right"></i> <strong>Email:</strong> <span>email@example.com</span></li>
+                    <li><i className="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                  </ul>
+                </div>
+              </div>
+              <p className="py-3">
+                Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
+                Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Stats />
+      <Siklls />
     </>
   )
 }
